83893be43d8dd065e7489859cf794dbdb3e0ce6a
gogl92 <luisarmando1234@gmail.com>
2015-08-07 23:40:54 -0500

    Yii and composer updated.

---
+++ vendor/jamband/yii2-schemadump/Migration.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of yii2-schemadump.
+ *
+ * (c) Tomoki Morita <tmsongbooks215@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace jamband\schemadump;
+
+use Yii;
+
+/**
+ * Migration class file.
+ */
+class Migration extends \yii\db\Migration
+{
+    /**
+     * @var string the table options
+     */
+    protected $tableOptions = '';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        if (Yii::$app->db->driverName === 'mysql') {
+            $this->tableOptions = 'ENGINE=InnoDB CHARACTER SET=utf8 COLLATE=utf8_unicode_ci';
+        }
+    }
+}
